0b4f65e3d1f954f14d3a1296ed8c43d3a5e194fc
mptich <mptich@gmail.com>
2016-08-27 11:19:02 -0700

    Added UrlRead.py

---
+++ pyutils/UrlRead.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import urllib2
+from urllib2 import URLError, HTTPError
+
+from StreetViewXMLParse import *
+
+def urlRead(urlStr, fileName = None):
+    data = None
+    file = None
+
+    try:
+        file = urllib2.urlopen(urlStr)
+        data = file.read()
+
+    except HTTPError as e:
+        print("getText: URL %s caused error %s code %d\n" %
+            (urlStr, repr(e), e.code))
+        return None
+
+    except (URLError, IOError) as e:
+        print("getText: URL %s caused error %s\n" % (urlStr, repr(e)))
+        return None
+        
+    if file:
+        file.close()
+    if fileName and data:
+        with open(fileName, "wb") as f:
+            f.write(data)
+    return data
+
+
+def test():
+
+    print urlRead("http://cbk0.google.com/cbk?output=xml&panoid=Q6Z2Rhy5_lHSoFAWz-Qmww")
+    urlRead("http://cbk0.google.com/cbk?output=thumbnail&w=300&h=128&panoid=Q6Z2Rhy5_lHSoFAWz-Qmww", "image2.jpeg")
+
+if __name__ == "__main__":
+    test()
