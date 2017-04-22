ddc1501e88251aecbcca5db3caa6741c36c3b45d
Misha Orel <mptich@gmail.com>
2017-04-22 11:13:37 +0300

    Added handling of reduce function between python 2 and python 3

--- pyutils/forwardCompat.py
+++ pyutils/forwardCompat.py
@@ -9,7 +9,9 @@ usingVersion3 = (sys.version_info[0] == 3)
 
 if not usingVersion3:
     from future.utils import viewitems
+    reduce = reduce
     maxint = sys.maxint
 else:
+    from functools import reduce
     maxint = sys.maxsize
 
