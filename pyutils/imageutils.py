c074fe56914ce5aff24790a00fd9ca0ae49cf4a1
Misha Orel <mptich@gmail.com>
2017-12-16 01:10:32 -0800

    Added ImageAnnotPlot Removed CVImage

--- pyutils/imageutils.py
+++ pyutils/imageutils.py
@@ -477,298 +477,56 @@ class ImageAnnot(UtilObject):
         return np.array(self.transpImage)
 
 
+class ImageAnnotPlot(UtilObject):
 
-class CVImage(UtilObject):
-    """
-    Image in the format convenient for CV processing
-    """
-
-    def __init__(self, image):
-        self.cleanup()
-        if isinstance(image, CVImage):
-            data = np.copy(image.data)
-        else:
-            assert isinstance(image, np.ndarray)
-            data = image
-
-        # Normalize
-        meanVal = np.mean(data, axis=(0,1))
-        stdVal = np.std(data, axis = (0,1)).clip(min=UtilNumpyClippingValue(np.float32))
-        self.data = np.multiply(data - meanVal, np.reciprocal(stdVal))
-
-    def cleanup(self):
-        self.fc = None
-        self.fcFine = None
-        self.fcNorm = None
-        self.fcFineNorm = None
-        self.fine = None
-        self.meanCell = None
-        self.neibStd = None
-        self.neibCells = None
-        self.neibCellsFine = None
-
-    def quickSubsample(self):
-        data = self.data
-        h, w, _ = np.shape(self.data)
-        hend = h if (h & 1) == 0 else h-1
-        wend = w if (w & 1) == 0 else w-1
-        self.data = (data[0:hend:2,0:wend:2,:]+data[0:hend:2,1::2,:]+data[1::2,0:wend:2,:]+data[1::2,1::2,:]) / 4.0
-
-    def subsample(self, times):
-        h, w, _ = np.shape(self.data)
-        img = self.image()
-        newSize = (int(h/times), int(w/times)) # This order for scipy
-        data = scipy.misc.imresize(img, newSize, interp="lanczos", mode="RGB")
-        self.__init__(image=data)
-
-    def gaussian(self, sigma):
-        self.data = np.dstack([scipyFilters.gaussian_filter(self.data[:,:,i], sigma=sigma) for i in range(3)])
-
-    def image(self, imageName=None):
-        return UtilDbgMatrixToImage(self.data, imageName=imageName, method ="direct")
+    def __init__(self, totalHeight, totalWidth, img):
+        self.height = totalHeight
+        self.width = totalWidth
+        plt.figure(figsize=(self.width/100, self.height/100), dpi=100)
+        img = UtilImageToInt(img)
+        plt.imshow(img)
+        plt.xlim((0, img.shape[1]))
+        plt.ylim((img.shape[0], 0))
 
     @staticmethod
-    def weightRing(radius):
-        """
-        Returns tuple (lc, ld), where lc is a list of tuples (x,y), and ld is a list of weights, for all pixels
-        within radius
-        """
-        lc = []
-        ld = []
-        r = int(radius)
-        rsq = radius * radius
-        for i in range(-r, r+1):
-            for j in range(-r, r+1):
-                dsq = float(i*i + j*j)
-                if rsq >= dsq:
-                    lc.append((i, j))
-                    #ld.append(rsq / (rsq + dsq))
-                    ld.append(1.)
-        return (lc, ld)
+    def scaleColor(color):
+        return tuple(x / 255. for x in color)
 
     @staticmethod
-    def linearFeat(x, y):
-        return [1., y, x]
+    def pointPairToLineCoord(p1, p2):
+        return (np.array([p1[1], p2[1]]), np.array([p1[0], p2[0]]))
 
-    @staticmethod
-    def squareFeat(x, y):
-        return [1., y, x, y*y, x*y, x*x]
-
-    @staticmethod
-    def cubicFeat(x, y):
-        return [1., y, x, y*y, x*y, x*x, y*y*y, x*y*y, x*x*y, x*x*x]
-
-    @staticmethod
-    def quadFeat(x, y):
-        return [1., y, x, y*y, x*y, x*x, y*y*y, x*y*y, x*x*y, x*x*x, y*y*y*y, x*y*y*y, x*x*y*y, x*x*x*y, x*x*x*x]
-
-    def cells(self, radius, featFunc):
-        self.cleanup()
-        radint = int(radius)
-        wring = self.weightRing(radius)
-        weights = wring[1]
-        nSamples = len(weights)
-        nFeatures = len(featFunc(0., 0.))
-        features = np.ndarray((nSamples, nFeatures), dtype=np.float32)
-        featureCoord = np.ndarray((nSamples, 2), dtype=np.float32)
-        for i, e in enumerate(wring[0]):
-            x, y = (float(v) for v in e)
-            features[i,:] = featFunc(x, y)
-            featureCoord[i] = [x,y]
-        self.featFunc = featFunc
-        self.nFeatures = nFeatures
-        self.nSamples = nSamples
-        self.weights = weights
-        self.features = features
-        self.featureCoord = featureCoord
-        self.radius = radius
-        self.radint = radint
-
-        print ("Initial nSamples %s" % nSamples)
-
-        baseShape = np.shape(self.data)[0:2]
-        viewWidth = baseShape[1] - 2 * radint
-        viewHeight = baseShape[0] - 2 * radint
-        viewHorEnd = baseShape[1] - radint
-        viewVertEnd = baseShape[0] - radint
-        self.viewWidth = viewWidth
-        self.viewHeight = viewHeight
-        self.viewHorEnd = viewHorEnd
-        self.viewVertEnd = viewVertEnd
-
-        # Collect colors of the neighbouring cells
-        neibCells = np.stack([self.data[radint+y:viewVertEnd+y,radint+x:viewHorEnd+x] for x,y in wring[0]], axis=3)
-        self.meanCell, self.neibStd, self.neibCells = self.normalizeCells(neibCells)
-
-    @staticmethod
-    def correlate(cells1, cells2, upperLeft, lowerRight, std1=None, std2=None):
+    def addConnectedPoints(self, points, connections, color):
         """
-        :param cells1, cells2: 4D arrays (height,width,color,values)
-        :param upperLeft: (dx, dy) upper left corner of movement
-        :param lowerRight: (dx, dy) lower right corner of movement
-        :param std1, std2: if present, show per color STD of the original array
-        :return: 2 4-D array, showing correlations, and weights of these correlations,
-            in terms of (dx, dy), for each pixel. Weights array migt be None (then all correlations are of
-            equal weight)
-        """
-
-        viewHeight, viewWidth, nColors, nSamples = np.shape(cells1)
-        viewHeightOther, viewWidthOther, nColorsOther, nSamplesOther = np.shape(cells2)
-        gridRatio = viewHeightOther / viewHeight
-        if (viewHeight * gridRatio != viewHeightOther) or (viewWidth * gridRatio != viewWidthOther):
-            raise ValueError("Wronf axes 0/1 in correlate: (%d, %d) vs (%d, %d)" % \
-                             (viewHeight, viewWidth, viewHeightOther, viewWidthOther))
-        if (nColors != 3) or (nColorsOther != 3):
-            raise ValueError("In correlate: wrong number of colors: %d %d" % (nColors, nColorsOther))
-        if (nSamples != nSamplesOther):
-            raise ValueError("In correlate: nSamples are different: %d %d" % (nSamples, nSamplesOther))
-
-        minX, minY = upperLeft
-        maxX, maxY = lowerRight
-        startHorView = -minX if minX < 0 else 0
-        startOtherHor = minX if minX > 0 else 0
-        endHorView = viewWidth - maxX if maxX > 0 else viewWidth
-        startVertView = -minY if minY < 0 else 0
-        startOtherVert = minY if minY > 0 else 0
-        endVertView = viewHeight - maxY if maxY > 0 else viewHeight
-        if (startHorView >= endHorView) or (startVertView >= endVertView):
-            raise ValueError("correlateFine: incorrect coordinates: %s %s" % (repr(upperLeft), repr(lowerRight)))
-        minXFine,maxXFine,minYFine,maxYFine = (gridRatio*x for x in (minX,maxX,minY,maxY))
-        startHorViewFine,startOtherHorFine,endHorViewFine,startVertViewFine,startOtherVertFine,endVertViewFine = \
-            (gridRatio*x for x in (startHorView,startOtherHor,endHorView,startVertView,startOtherVert,endVertView))
-
-        tensor1 = cells1[startVertView:endVertView, startHorView:endHorView]
-        output = np.empty((endVertView-startVertView, endHorView-startHorView, maxYFine-minYFine, maxXFine-minXFine))
-
-        for j in range(maxYFine-minYFine):
-            for i in range(maxXFine-minXFine):
-                tensor2 = cells2[ \
-                    startOtherVertFine+j:startOtherVertFine+j+endVertViewFine-startVertViewFine:gridRatio, \
-                    startOtherHorFine+i:startOtherHorFine+i+endHorViewFine-startHorViewFine:gridRatio]
-                diff = tensor2 - tensor1
-                output[:,:,j,i] = np.sum(diff * diff, axis=(2,3))
-
-        weight = None
-        if (std1 is not None) and (std2 is not None):
-            weight1 = np.prod(std1, axis=2)
-            weight2 = np.prod(std2, axis=2)
-            weight = np.where(weight1 < weight2, x=weight1, y=weight2)
-
-        return (output, weight)
-
-    def localMinima(self, corr, weight=None):
-        """
-        :param corr: correlation matrix returned from correlate()
-        :param weight: weight returned from correlate
-        :return: 4-D ndarray containing True for local minimas
-        """
-
-        # First find the cut-off level. Let's have it at 10% of correlation values
-        count = 1000
-        temp = np.random.choice(corr.flatten(), count)
-        temp.sort()
-        corrLocalMinimaThreshold = temp[count / 10]
-        localMinima = np.logical_and( \
-            corr[:,:,1:-1,1:-1] < corr[:,:,:-2,1:-1], corr[:,:,1:-1,1:-1] < corr[:,:,1:-1,:-2])
-        localMinima = np.logical_and(localMinima, corr[:,:,1:-1,1:-1] < corr[:,:,2:,1:-1])
-        localMinima = np.logical_and(localMinima, corr[:,:,1:-1,1:-1] < corr[:,:,1:-1,2:])
-        localMinima = np.logical_and(localMinima, corr[:,:,1:-1,1:-1] < corrLocalMinimaThreshold)
-        return localMinima
-
-    def fineCells(self, fine):
-        """
-        Creates a fine structure out of neibCells, by extrapolating via a polinomial
-        :param fine: fine=1 yields 3x fine structure, fine=2 - 5x, fine=3 - 7x, etc
+        :param points: point coordinates, shape (N, 2), type float
+        :param connections: connections in terms of points, shape (K, 2), type uint
+        :param color: (R, G, B) 0 - 255
         :return:
         """
-        self.fine = fine
-        # First, approximate neibCells with a polinomial
-        nTargets = self.viewWidth * self.viewHeight * 3
 
-        neibCells = self.neibCells
-        labels = np.transpose(neibCells, axes=(3,0,1,2)).reshape(self.nSamples, nTargets)
+        color = self.scaleColor(color)
+        for p in points:
+            plt.plot(p[1], p[0], 'o', color=color)
+        pointPairs = points[connections]
+        for pp in pointPairs:
+            self.pointPairToLineCoord(pp[0], pp[1])
+            plt.plot(*self.pointPairToLineCoord(pp[0], pp[1]), color=color, linestyle='-', linewidth=1)
+        for ind, p in enumerate(points):
+            plt.text(p[1], p[0], ' ' + str(ind + 1), color=color, fontsize=self.height / 100)
 
-        reg = linear_model.LinearRegression(fit_intercept=False, n_jobs=-1)
-        reg.fit(self.features, labels, sample_weight=self.weights)
+    def addConnectedAndSecondaryPoints(self, points, secPoints, connections, color, secColor):
+        assert points.shape[0] == secPoints.shape[0]
+        self.addConnectedPoints(points, connections, color)
+        secColor = self.scaleColor(secColor)
+        for p in secPoints:
+            plt.plot(p[1], p[0], 'o', color=secColor)
+        for ind in range(points.shape[0]):
+            plt.plot(*self.pointPairToLineCoord(points[ind], secPoints[ind]),
+                     color=secColor, linestyle='-', linewidth=1)
 
-        fc = reg.coef_.reshape((self.viewHeight, self.viewWidth, 3, self.nFeatures))
-        self.fc = fc
-
-        # Now create the fine structure of neighbour cells
-        gridRatio = 1 + 2 * fine
-        smallRadius = self.radius - 1. # Take subset of the samples
-        smallFeatCoord = np.array([e for e in self.featureCoord if (e[0]*e[0]+e[1]*e[1]) <= smallRadius*smallRadius])
-        sampleCount = len(smallFeatCoord)
-        neibCellsFine = np.empty((self.viewHeight * gridRatio, self.viewWidth * gridRatio, 3, sampleCount))
-        for i in range(-fine,fine+1):
-            for j in range(-fine,fine+1):
-                xOff = float(i) / gridRatio
-                yOff = float(j) / gridRatio
-                features = np.array([self.featFunc(x+xOff,y+yOff) for x,y in smallFeatCoord])
-                neibCellsFine[fine+j::gridRatio, fine+i::gridRatio] = np.tensordot(fc, features, axes = [[3],[1]])
-        self.meanCellFine, self.neibStdFine, self.neibCellsFine = self.normalizeCells(neibCellsFine)
-
-    @staticmethod
-    def normalizeCells(cells):
-        """
-        :param cells: 4D array of cells (height,width,color,values)
-        :return: tuple (meanCells(height,width,color), std(height,width,color), normilizedCells(height,width,
-                color,values)
-        """
-        meanCell = np.mean(cells, axis=(3,))
-        diffCells = cells - meanCell.reshape(np.shape(meanCell) + (1,))
-        std = np.std(diffCells, axis=(3,)).clip(min=UtilNumpyClippingValue(np.float32))
-        cells = np.multiply(diffCells, np.reciprocal(std).reshape((np.shape(std) + (1,))))
-        return (meanCell, std, cells)
-
-
-    @staticmethod
-    def neibCells2D(neibCells, featureCoord):
-        """
-        :param neibCells: array of neibCells, size of self.nSamples
-        :return: neibCells as 2D array; pixels not belonging to it a 0s
-        """
-        radint = int(np.max(featureCoord))
-        size = radint * 2 + 2 # extra 1 for the black border
-        if len(np.shape(neibCells)) < 3:
-            # If the 2st 2 dimensions are missing, amke them 1 "pixel"
-            neibCells = neibCells.reshape((1,1)+np.shape(neibCells))
-        if len(np.shape(neibCells)) < 4:
-            # If colors are missing
-            x,y,z = np.shape(neibCells)
-            neibCells = np.repeat(neibCells.reshape((x,y,1,z)), repeats=3, axis=2)
-        neibCells = np.transpose(neibCells, axes=(0,1,3,2))
-        shape = np.shape(neibCells)
-        nSamples = len(featureCoord)
-        assert nSamples == shape[2]
-        minVal = np.min(neibCells)
-        maxVal = np.max(neibCells)
-        missingVal = minVal - 0.3 * (maxVal-minVal)
-        ret = np.empty((shape[0], size, shape[1], size, 3), dtype=np.float32)
-        ret.fill(missingVal)
-        for k in range(nSamples):
-            i,j = (int(x) for x in featureCoord[k])
-            ret[:,radint+j,:,radint+i,:] = neibCells[:,:,k,:]
-        ret = ret.reshape((shape[0]*size, shape[1], size, 3)).reshape((shape[0]*size, shape[1]*size, 3))
-        return ret
-
-    def edge(self):
-        """
-        Use Sobel filters
-        :return: matrix (height, width) of detected edges
-        """
-        horFilt = np.array([[-1,0,1],[-2,0,2],[-1,0,1]], dtype=np.float32)
-        vertFilt = np.array([[-1,-2,-1],[0,0,0],[1,2,1]], dtype=np.float32)
-        horEdges = np.dstack([scipyFilters.convolve(self.data[:,:,i], horFilt) for i in range(3)])
-        vertEdges = np.dstack([scipyFilters.convolve(self.data[:,:,i], vertFilt) for i in range(3)])
-        horEdges = np.sum(horEdges * horEdges, axis=2)
-        vertEdges = np.sum(vertEdges * vertEdges, axis=2)
-        self.edges = np.sqrt(horEdges + vertEdges)
-        return UtilDbgMatrixToImage(self.edges)
-
-    def meanSharpness(self):
-        h,w = self.edges.shape
-        return np.sum(self.edges) / (h*w)
+    def save(self, fileName):
+        plt.savefig(fileName)
+        plt.close()
 
 
 class BoundingBox(UtilObject):
@@ -844,6 +602,8 @@ class BoundingBoxStats(UtilObject):
 
     def getStat(self, name, statList):
         return (np.mean(statList), np.std(statList))
+
+
 
 
 
