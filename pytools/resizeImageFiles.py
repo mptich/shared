9e50fa926190c07e7fa6000738be4eb2a817f944
Mike Orel <morel@morel-mac.attlocal.net>
2018-09-24 01:06:21 -0700

    More debug output

--- pytools/resizeImageFiles.py
+++ pytools/resizeImageFiles.py
@@ -8,20 +8,27 @@ from shared.pyutils.imageutils import *
 
 
 def _usage():
-  print('resize_image_files.py <input_pattern> <output_dir> <HxW>')
+  print('resize_image_files.py <input_dir> <output_dir> <HxW>')
   sys.exit() 
 
 try:
-  inputPattern = sys.argv[1]
+  inputDir = sys.argv[1]
   outputDir = sys.argv[2]
   reso = sys.argv[3]
   height, width = (int(x) for x in reso.split('x'))
 except:
   _usage()
 
-for fn in glob.glob(input_pattern):
+counter = 0
+for fn in glob.glob(inputDir + '/*.jpg'):
   _, fnBase = os.path.split(fn)
   outFn = outputDir + '/' + fnBase 
   img = UtilImageFileToArray(fn)
+  if img is None:
+    print('CORRUPTED: %s' % fn)
+    continue
   img = UtilImageResize(img, height, width)
   UtilArrayToImageFile(img, outFn)
+  counter += 1
+  if counter % 1000 == 0:
+    print(counter)
